63e24f082cd0129b367b0a48618f4022ccf41f9a
grhav <grhav@DESKTOP-1335TBB>
2021-02-20 21:41:25 +1100

    changes

--- MavenTestngGithubJankins/src/test/java/project/Demo.java
+++ MavenTestngGithubJankins/src/test/java/project/Demo.java
@@ -7,6 +7,17 @@ public class Demo {
 
 	@Test
 	public void testA() {
+		Reporter.log("Hi",true);		
+	}
+	
+	@Test
+	public void testB() {
+		Reporter.log("Hi",true);
+		//hi
+	}
+	
+	@Test
+	public void testC() {
 		Reporter.log("Hi",true);
 		//hi
 	}
