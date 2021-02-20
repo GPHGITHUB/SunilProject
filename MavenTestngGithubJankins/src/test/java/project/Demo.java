63bb404e2af255807ee5bcf79d442200808c3f54
grhav <grhav@DESKTOP-1335TBB>
2021-02-20 23:13:22 +1100

    comments to update chnages

--- MavenTestngGithubJankins/src/test/java/project/Demo.java
+++ MavenTestngGithubJankins/src/test/java/project/Demo.java
@@ -1,4 +1,10 @@
 package project;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 //sample TestNG Class
 import org.testng.Reporter;
 import org.testng.annotations.Test;
@@ -18,6 +24,22 @@ public class Demo {
 	@Test
 	public void testR() {
 		Reporter.log("Hi",true);
+	}
+	
+	@Test
+	public void testA() throws MalformedURLException {
+		Reporter.log("Hi",true);
+		//WebDriver driver=new ChromeDriver(); //this code will open chrome browser 
+		//in current computer where code is present
+		URL remoteAddress=new URL("http://localhost:4444/wd/hub");
+		
+		DesiredCapabilities capabilities=new DesiredCapabilities();
+		capabilities.setBrowserName("chrome");
+		
+		WebDriver driver=new RemoteWebDriver(remoteAddress, capabilities);//this code will open the browser 
+		//in remote system (node)
+		driver.get("http://www.google.com");
+		driver.quit();
 	}
 	
 }
